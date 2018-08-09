29540fc1a615b01ebff6bed20c3f0554423419a8
foresitegroup <mark@foresitegrp.com>
2018-08-09 17:12:57 -0500

    2018-08-09

---
+++ about.php
@@ -0,0 +1,81 @@
+<?php
+$PageTitle = "About";
+include "header.php";
+?>
+
+<div id="page-title">
+  <div class="site-width">
+    About Armour Coatings, Inc.
+  </div>
+</div>
+
+<div id="about">
+  <div class="site-width">
+    <div>
+      Founded in 2003 and located in Germantown Wisconsin, Armour Coatings is a multi-metal job shop custom powder coater and sandblaster. Armour Coatings operates from a 45,000 sq. ft. facility that employs the latest technologies for part cleaning, powder delivery and curing of finishes.<br>
+      <br>
+
+      Armour Coating's commitment to quality ensures stable growth and profitability. We are dedicated to providing powder coating and sandblasting services that exceed our client's expectations. We offer honest and eco-friendly powder coating solutions with a pledge to protect the environment. We measure success in our great relationships with clients, employees, vendors, and the community.
+    </div>
+  </div>
+
+  <img src="images/truck.png" alt="">
+</div>
+
+<div id="header-black">
+  <div class="site-width">
+    Facilities &amp; Equipment
+  </div>
+</div>
+
+<div id="facilities">
+  <div class="site-width">
+    <div>
+      <h3>3 Powder Coating Production Lines</h3>
+
+      <h4>Line 1 - Manual Powder Application</h4>
+      Mixed metals including hot and cold rolled steel, aluminum, castings, brass, stainless steel, etc.<br>
+      <br>
+
+      <h4>Line 2 Batch System Manual Application Powder</h4>
+      10,000 sq. ft. containing a large part blasting booth as well as a small part blasting cabinet.<br>
+      <br>
+
+      <h4>Line 3 Automatic Powder Application with Touch Up Gun</h4>
+      54 to 80 Grit Aluminum Oxide (Large Blast Booth)<br>
+      Glass Bead (Small Part Blast Cabinet)
+    </div>
+
+    <div>
+      <h4>Facility</h4>
+      45,000 sq. ft.<br>
+      <br>
+
+      <h4>Pretreatment</h4>
+      Six Stage Wash Systems with Laser De-scale capability<br>
+      <br>
+
+      <h4>Substrates Handled</h4>
+      Mixed metals including hot and cold rolled steel, aluminum, castings, brass, stainless steel, etc.<br>
+      <br>
+
+      <h4>Sandblasting Facility</h4>
+      10,000 sq. ft. containing a large part blasting booth as well as a small part blasting cabinet.<br>
+      <br>
+
+      <h4>Blasting Media</h4>
+      54 to 80 Grit Aluminum Oxide (Large Blast Booth)<br>
+      Glass Bead (Small Part Blast Cabinet)
+    </div>
+  </div>
+</div>
+
+<div id="request-quote">
+  <div class="site-width">
+    <div>Ready to get started?</div>
+
+    <a href="contact.php">Request a Quote</a>
+  </div>
+</div>
+
+<?php include "footer.php"; ?>
--- process.php
+++ process.php
@@ -54,8 +54,46 @@ include "header.php";
 </div>
 
 <div id="supporting">
-  <div class="site-width">
-    [Supporting goes here]
+  <div>
+    <div class="site-width">
+      <div>
+        <div>
+          <h2>Pre-Treatment</h2>
+          Our overhead conveyor lines utilize a six-stage wash system capable of surpassing AAMA 2605-5 and ANSI C57.12.28 specifications, which includes Alkaline and Laser Oxide Removal as needed. It is comparable to traditional Iron Phosphate Systems, but uses nanotechnologies to provide pretreatment for many substrates such as; Steel, Aluminum, Copper, Brass, Etc. Chemical titration is controlled automatically. Our pretreatment wash system has been approved through several large OEMs.
+        </div>
+      </div>
+    </div>
+
+    <img src="images/process-pre-treatment.jpg" alt="">
+  </div>
+
+  <div class="right">
+    <div class="site-width">
+      <div>
+        <div>
+          <h2>Curing</h2>
+          Stand alone or inline natural gas convection/infrared cure ovens are in place to handle this critical stage of the powder coating process. Powder Manufactures set the specifications, as to the oven temperature and duration of time that any product is in the oven to achieve the desired coating properties for chemical resistance, hardness and UV resistance.<br>
+          <br>
+
+          Along with our inline curing oven capabilities, Armour Coatings can accommodate large pieces as well in our Stand-alone system. Please contact us to see our capabilities.
+        </div>
+      </div>
+    </div>
+
+    <img src="images/process-curing.jpg" alt="">
+  </div>
+
+  <div>
+    <div class="site-width">
+      <div>
+        <div>
+          <h2>Quality Control</h2>
+          Armour Coatings' Quality Control Department uses state of the art testing and documentation to assure a uniform finish. QC maintains process records and photos of each commonly run part, which are stored in a matrix for future parts runs helping ensure repetitive quality.
+        </div>
+      </div>
+    </div>
+
+    <img src="images/process-quality-control.jpg" alt="">
   </div>
 </div>
 
